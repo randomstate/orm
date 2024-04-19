d07dfe93faac51003873c2f04d38e3e84bbcb1b5
Connor Forsyth <connor@randomstate.co.uk>
2024-04-19 09:52:58 +0100

    Remove fluent driver

--- tests/LaravelTest.php
+++ tests/LaravelTest.php
@@ -7,6 +7,7 @@ namespace RandomState\Orm\Tests;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Illuminate\Support\Str;
+use PHPUnit\Framework\Attributes\Test;
 use RandomState\Orm\OrmServiceProvider;
 use Tests\TestCase;
 
@@ -14,15 +15,14 @@ class LaravelTest extends TestCase
 {
     protected function setUp(): void
     {
+        $_ENV['APP_BASE_PATH'] = __DIR__ . '/../vendor/laravel/laravel';
         parent::setUp();
 
         $this->app['config']->set('app.key', Str::random(32));
         $this->app->register(OrmServiceProvider::class);
     }
 
-    /**
-     * @test
-     */
+    #[test]
     public function can_instantiate_entity_manager()
     {
         $em = $this->app->make(EntityManagerInterface::class);
@@ -30,12 +30,14 @@ class LaravelTest extends TestCase
         $this->assertInstanceOf(EntityManagerInterface::class, $em);
     }
 
-    /**
-     * @test
-     */
+    #[test]
     public function laravel_boots_up()
     {
-        $this->get('/')
+        config()->set('session.driver', 'file');
+
+        $this
+            ->withoutExceptionHandling()
+            ->get('/')
             ->assertok();
     }
 }
